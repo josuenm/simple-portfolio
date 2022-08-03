022ec763473ce00efc939975f19c4fb013800bc0
josuenm <direct.josue@gmail.com>
2022-08-03 17:29:13 -0300

    Componente fonts e theme para o theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import Layout from '@components/layouts/Main'
 import { AppProps } from 'next/app'
+import Layout from '@components/layouts/Main'
+import theme from '@styles/theme'
+import Fonts from '@styles/fonts'
 
 const Website = ({ Component, pageProps, router }: AppProps) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
