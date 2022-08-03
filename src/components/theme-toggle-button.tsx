0fdacbbd92d356287fefefe09ad7aaeeb422d622
josuenm <direct.josue@gmail.com>
2022-08-03 20:27:33 -0300

    Animação com framer-motion

--- src/components/theme-toggle-button.tsx
+++ src/components/theme-toggle-button.tsx
@@ -1,16 +1,26 @@
 import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
+import { motion } from 'framer-motion'
 
 const ThemeToggleButton = () => {
   const { toggleColorMode } = useColorMode()
 
   return (
-    <IconButton
-      aria-label='Trocar tema'
-      colorScheme={useColorModeValue('purple', 'orange')}
-      icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-      onClick={toggleColorMode}
-    ></IconButton>
+    <motion.div
+      style={{ display: 'inline-block' }}
+      key={useColorModeValue('light', 'dark')}
+      initial={{ y: -20, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: 20, opacity: 0 }}
+      transition={{ duration: 0.2 }}
+    >
+      <IconButton
+        aria-label='Trocar tema'
+        colorScheme={useColorModeValue('purple', 'orange')}
+        icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+        onClick={toggleColorMode}
+      />
+    </motion.div>
   )
 }
 
