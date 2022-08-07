6f9e3a9aa1d88bb075d0071b91430672773796a4
josuenm <direct.josue@gmail.com>
2022-08-07 18:19:53 -0300

    Link para o github

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -29,7 +29,7 @@ interface NavbarProps {
 
 const LinkItem = ({ href, path, children }: LinkItemProps) => {
   const active = path === href
-  const inactiveColor = useColorModeValue('gray.200', 'whiteAlpha.900')
+  const inactiveColor = useColorModeValue('gray.900', 'whiteAlpha.900')
 
   return (
     <NextLink href={href}>
@@ -37,6 +37,7 @@ const LinkItem = ({ href, path, children }: LinkItemProps) => {
         p={2}
         bg={active ? 'glassTeal' : undefined}
         color={active ? '#202023' : inactiveColor}
+        borderRadius={5}
       >
         {children}
       </Link>
@@ -64,9 +65,9 @@ const Navbar = (props: NavbarProps) => {
         flexWrap='wrap'
         alignItems='center'
         justifyContent='space-between'
-        gap={5}
+        gap={6}
       >
-        <Flex alignItems='center' flex={1}>
+        <Flex alignItems='center'>
           <Heading as='h1' size='lg' letterSpacing='tighter'>
             <Logo />
           </Heading>
@@ -74,16 +75,26 @@ const Navbar = (props: NavbarProps) => {
         <Stack
           direction={{ base: 'column', md: 'row' }}
           display={{ base: 'none', md: 'flex' }}
+          flex={1}
           width={{ base: 'full', md: 'auto' }}
           alignItems='center'
           mt={{ base: 4, md: 0 }}
         >
-          <LinkItem href='/projects' path={path}>
-            Projects
+          <LinkItem href='/works' path={path}>
+            Trabalhos
           </LinkItem>
           <LinkItem href='/social' path={path}>
             Social
           </LinkItem>
+          <NextLink href=''>
+            <Link
+              color={useColorModeValue('gray.900', 'whiteAlpha.900')}
+              href='https://github.com/josuenm'
+              target='_blank'
+            >
+              Github
+            </Link>
+          </NextLink>
         </Stack>
 
         <Flex align='center'>
@@ -94,6 +105,8 @@ const Navbar = (props: NavbarProps) => {
                 as={IconButton}
                 icon={<HamburgerIcon />}
                 variant='outline'
+                borderWidth={2}
+                borderColor={useColorModeValue('#ddd', '#454545')}
                 arial-label='Opções'
               />
               <MenuList>
