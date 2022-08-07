a6fab74704adf03ed124135f7a9465e2b2c0aeb3
josuenm <direct.josue@gmail.com>
2022-08-07 18:18:39 -0300

    Componente layout em volta da página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,95 +13,98 @@ import {
   Text,
   useColorModeValue
 } from '@chakra-ui/react'
+import Layout from '@components/layouts/layout'
 
 const Home: NextPage = () => {
   return (
-    <Container>
-      <Box
-        bg={useColorModeValue('whiteAlpha.900', 'whiteAlpha.200')}
-        textAlign='center'
-        borderRadius='lg'
-        p={1.5}
-        mb={6}
-      >
-        <Text as='span'>
-          Olá, sou um desenvolvedor Full Stack de Jacareí, SP - Brasil
-        </Text>
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as='h2'>Josué Mendonça</Heading>
-          <Text as='p'>CactusOutlier (desenvolvedor, info-produtor)</Text>
+    <Layout>
+      <Container>
+        <Box
+          bg={useColorModeValue('whiteAlpha.900', 'whiteAlpha.200')}
+          textAlign='center'
+          borderRadius='lg'
+          p={1.5}
+          mb={6}
+        >
+          <Text as='span'>
+            Olá, sou um desenvolvedor Full Stack de Jacareí, SP - Brasil
+          </Text>
         </Box>
 
-        <Flex
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          justifyContent={{ base: 'center' }}
-          alignItems='center'
-        >
-          <Image
-            borderColor='whiteAlpha.800'
-            borderWidth={2}
-            borderStyle='solid'
-            maxW='100px'
-            display='inline-block'
-            borderRadius='full'
-            src='/images/profile-picture.jpg'
-          />
-        </Flex>
-      </Box>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as='h2'>Josué Mendonça</Heading>
+            <Text as='p'>Outlier (desenvolvedor full stack)</Text>
+          </Box>
 
-      <Section delay={0.1}>
-        <Heading as='h3' variant='section-title'>
-          Sobre mim
-        </Heading>
-        <Text as='p'>
-          Josué é um desenvolvedor full stack que procura sempre evoluir.
-          Atualmente esta dando um foco maior no aprendizado do back-end, mas
-          sua grande paixão no começo foi querer desenvolver para mobile mas
-          acabou adiando essa meta para focar em React na web e depois começar
-          em React Native 😎.
-        </Text>
+          <Flex
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            justifyContent={{ base: 'center' }}
+            alignItems='center'
+          >
+            <Image
+              borderColor='whiteAlpha.800'
+              borderWidth={2}
+              borderStyle='solid'
+              maxW='100px'
+              display='inline-block'
+              borderRadius='full'
+              src='/images/profile-picture.jpg'
+            />
+          </Flex>
+        </Box>
 
-        <Flex justify='center' paddingY={5}>
-          <NextLink href='/projects'>
-            <Button colorScheme='messenger' rightIcon={<ChevronRightIcon />}>
-              Projetos
-            </Button>
-          </NextLink>
-        </Flex>
-      </Section>
+        <Section delay={0.1}>
+          <Heading as='h3' variant='section-title'>
+            Sobre mim
+          </Heading>
+          <Text as='p'>
+            Josué é um desenvolvedor full stack que procura sempre evoluir.
+            Atualmente esta dando um foco maior no aprendizado do back-end, mas
+            sua grande paixão no começo foi querer desenvolver para mobile mas
+            acabou adiando essa meta para focar em React na web e depois começar
+            em React Native 😎.
+          </Text>
 
-      <Section delay={0.1}>
-        <Heading as='h3' variant='section-title'>
-          Trabalho
-        </Heading>
-        <BioContainer>
-          <BioYear>2021 até agora</BioYear>
-          Trabalhando como Freelancer
-        </BioContainer>
-        <BioContainer>
-          <BioYear>2022 até agora</BioYear>
-          Produtor/vendedor de ebooks
-        </BioContainer>
-      </Section>
+          <Flex justify='center' pt={4}>
+            <NextLink href='/works'>
+              <Button colorScheme='teal' rightIcon={<ChevronRightIcon />}>
+                Trabalhos
+              </Button>
+            </NextLink>
+          </Flex>
+        </Section>
 
-      <Section delay={0.1}>
-        <Heading as='h3' variant='section-title'>
-          Gostos
-        </Heading>
-        <Text as='p'>Formula 1, músicas, jogos na nuvem e programar</Text>
-      </Section>
+        <Section delay={0.1}>
+          <Heading as='h3' variant='section-title'>
+            Trabalho
+          </Heading>
+          <BioContainer>
+            <BioYear>2021 até agora</BioYear>
+            Trabalhando como Freelancer
+          </BioContainer>
+          <BioContainer>
+            <BioYear>2022 até agora</BioYear>
+            Produtor/vendedor de ebooks
+          </BioContainer>
+        </Section>
 
-      <Section delay={0.1}>
-        <Heading as='h3' variant='section-title'>
-          Pela internet
-        </Heading>
-      </Section>
-    </Container>
+        <Section delay={0.1}>
+          <Heading as='h3' variant='section-title'>
+            Gostos
+          </Heading>
+          <Text as='p'>Formula 1, músicas, jogos na nuvem e programar</Text>
+        </Section>
+
+        <Section delay={0.1}>
+          <Heading as='h3' variant='section-title'>
+            Pela internet
+          </Heading>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
