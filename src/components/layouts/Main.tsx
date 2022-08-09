199079f8224ef8f7d621414a90c9ade0936a48f7
josuenm <direct.josue@gmail.com>
2022-08-08 21:06:02 -0300

    Return deletado, footer adicionado

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -1,28 +1,38 @@
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Text, useColorModeValue } from '@chakra-ui/react'
 import { NextRouter } from 'next/router'
 import Head from 'next/head'
 import Navbar from '@components/navbar'
+import Penguin from '@components/penguin'
 
 interface MainProps {
   children: React.ReactNode[] | React.ReactNode
   router: NextRouter
 }
 
-const Main = ({ children, router }: MainProps) => {
-  return (
-    <Box as='main' pb='8'>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <title>Josué Mendonça | Homepage</title>
-      </Head>
+const Main = ({ children, router }: MainProps) => (
+  <Box as='main' pb='8'>
+    <Head>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <title>Josué Mendonça | Homepage</title>
+    </Head>
 
-      <Navbar path={router.asPath} />
+    <Navbar path={router.asPath} />
 
-      <Container maxW='container.md' pt='32' pb='16'>
-        {children}
-      </Container>
-    </Box>
-  )
-}
+    <Container maxW='container.md' pt='14' pb='1'>
+      <Penguin />
+      {children}
+
+      <Box as='footer' textAlign='center' mt={10}>
+        <Text
+          fontWeight='light'
+          fontSize={14}
+          color={useColorModeValue('gray.500', 'whiteAlpha.400')}
+        >
+          2022 Josué Mendonça &copy; Todos direitos reservados
+        </Text>
+      </Box>
+    </Container>
+  </Box>
+)
 
 export default Main
