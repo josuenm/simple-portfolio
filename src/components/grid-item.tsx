e6fd768c5e95c1d9c26f9599a9eff0254c792fe6
josuenm <direct.josue@gmail.com>
2022-08-08 21:06:49 -0300

    Return deletado

--- src/components/grid-item.tsx
+++ src/components/grid-item.tsx
@@ -14,30 +14,28 @@ export const WorkGridItem = ({
   title,
   thumbnail,
   children
-}: WorkGridItem) => {
-  return (
-    <Flex direction='column'>
-      <NextLink href={`works/${id}`}>
-        <LinkBox cursor='pointer'>
-          <Box position='relative' w='100%' h={[160, 250, 130]}>
-            <Image
-              src={thumbnail}
-              alt={title}
-              className='grid-item-thumbnail'
-              placeholder='blur'
-              blurDataURL='L6PZfSi_.AyE_3t7t7R**0o#DgR4'
-              style={{ borderRadius: '12px' }}
-              layout='fill'
-            />
-          </Box>
-          <Text mt={3} fontSize={20} textAlign='center'>
-            {title}
-          </Text>
-          <Text fontSize={14} textAlign='center'>
-            {children}
-          </Text>
-        </LinkBox>
-      </NextLink>
-    </Flex>
-  )
-}
+}: WorkGridItem) => (
+  <Flex direction='column'>
+    <NextLink href={`works/${id}`}>
+      <LinkBox cursor='pointer'>
+        <Box position='relative' w='100%' h={[160, 250, 130]}>
+          <Image
+            src={thumbnail}
+            alt={title}
+            className='grid-item-thumbnail'
+            placeholder='blur'
+            blurDataURL='L6PZfSi_.AyE_3t7t7R**0o#DgR4'
+            style={{ borderRadius: '12px' }}
+            layout='fill'
+          />
+        </Box>
+        <Text mt={3} fontSize={20} textAlign='center'>
+          {title}
+        </Text>
+        <Text fontSize={14} textAlign='center'>
+          {children}
+        </Text>
+      </LinkBox>
+    </NextLink>
+  </Flex>
+)
