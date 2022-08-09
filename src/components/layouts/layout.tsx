f1e0d3e6ea754f7bd59ac7cff9a320538e19555e
josuenm <direct.josue@gmail.com>
2022-08-08 21:04:33 -0300

    Return deletado

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -12,24 +12,22 @@ const variants = {
   exit: { opacity: 0, x: 0, y: 20 }
 }
 
-const Layout = ({ children, title }: LayoutProps) => {
-  return (
-    <motion.div
-      initial='hidden'
-      animate='enter'
-      exit='exit'
-      variants={variants}
-      transition={{ duration: 0.4, type: 'easeInOut' }}
-      style={{ position: 'relative' }}
-    >
-      {title && (
-        <Head>
-          <title>{title} | Josué Mendonça</title>
-        </Head>
-      )}
-      {children}
-    </motion.div>
-  )
-}
+const Layout = ({ children, title }: LayoutProps) => (
+  <motion.div
+    initial='hidden'
+    animate='enter'
+    exit='exit'
+    variants={variants}
+    transition={{ duration: 0.4, type: 'easeInOut' }}
+    style={{ position: 'relative' }}
+  >
+    {title && (
+      <Head>
+        <title>{title} | Josué Mendonça</title>
+      </Head>
+    )}
+    {children}
+  </motion.div>
+)
 
 export default Layout
