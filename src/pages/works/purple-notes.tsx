d514b17ab8de27a1aea7840d5f4f1c7d899974e5
josuenm <direct.josue@gmail.com>
2022-08-22 22:57:13 -0300

    Bug do idioma sumindo ao clicar no link foi consertado

--- src/pages/works/purple-notes.tsx
+++ src/pages/works/purple-notes.tsx
@@ -1,20 +1,31 @@
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Badge, Container, Link, List, ListItem } from '@chakra-ui/react'
 import Layout from '@components/layouts/layout'
 import P from '@components/paragraph'
-import { Badge, Container, Link, List, ListItem } from '@chakra-ui/react'
 import { Meta, Title, WorkImage } from '@components/work'
 import { GetStaticProps, GetStaticPropsContext, NextPage } from 'next'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 export const getStaticProps: GetStaticProps = async ({
   locale
 }: GetStaticPropsContext) => {
+  const projects = [
+    'spotify_homepage_clone',
+    'pretty_form',
+    'purple_notes',
+    'linkedin_clone',
+    'netflix_clone',
+    'magic_molds',
+    'mold_pack',
+    'simply_amigurumi'
+  ]
+
   return {
     props: {
       ...(await serverSideTranslations(locale as string, [
+        ...projects,
         'works',
-        'purple_notes',
         'header',
         'footer'
       ]))
