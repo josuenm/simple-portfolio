af0578dda3ba186ce90023ac7680135ca5bdab0b
josuenm <direct.josue@gmail.com>
2022-08-22 22:54:30 -0300

    Evitando o scroll para cima da página ao clicar nos links

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,29 @@
-import Layout from '@components/layouts/main'
-import theme from '@lib/theme'
-import Fonts from '@lib/fonts'
 import { ChakraProvider } from '@chakra-ui/react'
-import { AppProps } from 'next/app'
+import Layout from '@components/layouts/main'
+import Fonts from '@lib/fonts'
+import theme from '@lib/theme'
 import { AnimatePresence } from 'framer-motion'
 import { appWithTranslation } from 'next-i18next'
+import { AppProps } from 'next/app'
 
 const Website = ({ Component, pageProps, router }: AppProps) => {
+  if (typeof window !== 'undefined') {
+    window.history.scrollRestoration = 'manual'
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
       <Layout router={router}>
-        <AnimatePresence exitBeforeEnter initial={true}>
+        <AnimatePresence
+          exitBeforeEnter
+          initial={true}
+          onExitComplete={() => {
+            if (typeof window !== 'undefined') {
+              window.scrollTo({ top: 0 })
+            }
+          }}
+        >
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </Layout>
